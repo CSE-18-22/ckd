ae2f7f9df073abc29f85f38e970e0ec578c98d79
vinod reddy <vinod@vinods-MacBook-Pro.local>
2022-06-20 22:40:06 +0530

    destructure code

--- install.sh
+++ install.sh
@@ -1,10 +1,7 @@
-cd app/
 npm i
-cd ../api/
 brew install python3
 python3 -m venv venv
 pip install flask
 pip install -U flask_cors
 pip3 install -r requirements.txt
 export FLASK_APP=app
-cd ../
--- run.sh
+++ run.sh
@@ -1,5 +1,3 @@
-cd app
 npm run dev & 
-cd ../api
 . venv/bin/activate
 flask run
